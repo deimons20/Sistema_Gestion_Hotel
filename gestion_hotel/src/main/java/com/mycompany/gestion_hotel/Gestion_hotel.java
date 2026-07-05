13275ef25b52b112f6e72d59085cf7a9d6ae4135
Josue <u23249497@utp.edu.pe>
2026-07-05 18:27:44 -0500

    aw

---
+++ gestion_hotel/src/main/java/com/mycompany/gestion_hotel/Gestion_hotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion_hotel;
+
+/**
+ *
+ * @author rojas
+ */
+public class Gestion_hotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
